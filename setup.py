2cc282bf110626319f282b2cdbdcd08c290251d0
LibardoBT <leberduto@hotmail.com>
2025-05-14 04:54:14 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="releaseRepo",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="LibardoBT",                             # Tu nombre
+    author_email="leberduto@hotmail.com",           # Tu correo electrónico
+    url="https://github.com/LibardoBT/releaseRepo", # URL del proyecto
+)
